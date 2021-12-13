e9dc14e6683e5ecab6d764664970a0119fa40730
Pablo Matias Nader <hm.pablito@gmail.com>
2021-12-13 20:35:22 -0300

    CHANGE db.js

--- config/db.js
+++ config/db.js
@@ -1,8 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('swapi_clon', 'root', '', {
-  host: 'localhost',
-  dialect: 'mariadb'
+// const sequelize = new Sequelize('swapi_clon', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mariadb'
+// });
+
+// user: bf8ba3a4e303d4
+// password: 32947316
+// host: us-cdbr-east-05.cleardb.net
+// db: heroku_d3b4ab191c2ac85
+
+
+const sequelize = new Sequelize('heroku_d3b4ab191c2ac85', 'bf8ba3a4e303d4', '32947316', {
+  host: 'us-cdbr-east-05.cleardb.net',
+  dialect: 'mysql'
 });
 
 module.exports = sequelize;
