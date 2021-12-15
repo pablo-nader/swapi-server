e7259f9028ad1713ad13e497c6c1649cb56880f8
Pablo Matias Nader <hm.pablito@gmail.com>
2021-12-15 19:39:35 -0300

    CHANGE const to let vehicleController

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -93,7 +93,7 @@ exports.create = (req, res) => {
 
 // body
 exports.edit = (req, res) => {
-    const imgFile;
+    let imgFile;
     if (req.file) {
         imgFile = req.file.originalname.replace(/[ ()]/g, '');
     } else {
