93f520e3a495f3e596bfe8c04c73b93f46f41a07
Pablo Matias Nader <hm.pablito@gmail.com>
2021-12-19 20:03:54 -0300

    CHANGE image folder

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -117,9 +117,13 @@ exports.edit = (req, res) => {
 
     let id = req.params.id;
     if (!isNaN(id) && id > 0) {
-        Vehicle.findAndCountAll({ where: { name: req.body.name }})
+        Vehicle.findAndCountAll({ where: { 
+            name: req.body.name,
+            id: {
+                [Op.ne]: req.params.id
+            } }})
         .then(data => {
-            if (data.count !== 0 && data.rows[0].dataValues.id !== id) {
+            if (data.count !== 0) {
                 res.send({ details: "Name already exists" });
             }
         })
